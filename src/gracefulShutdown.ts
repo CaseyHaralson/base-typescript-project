8452e5d29d88c6011c86aabfe6b7f6fd01a3af07
Casey <casey.haralson@gmail.com>
2023-05-09 19:09:51 -0600

    updating the shutdown handler process

--- src/gracefulShutdown.ts
+++ src/gracefulShutdown.ts
@@ -1,20 +1,20 @@
-process.on('SIGINT', () => {
+process.once('SIGINT', () => {
   console.log(
     'Got SIGINT event. Starting graceful shutdown ',
     new Date().toISOString()
   );
-  shutdown();
+  shutdown('SIGINT');
 });
 
-process.on('SIGTERM', () => {
+process.once('SIGTERM', () => {
   console.log(
     'Got SIGTERM event. Starting graceful shutdown ',
     new Date().toISOString()
   );
-  shutdown();
+  shutdown('SIGTERM');
 });
 
-function shutdown() {
+function shutdown(signal: string) {
   // probably want to wrap the server in npm project "stoppable"
   // so it will handle any server connections
   // then
@@ -24,4 +24,5 @@ function shutdown() {
   // Note: you might need to extend the host timeout if we aren't
   // getting the "Graceful shutdown" log message
   console.log('Graceful shutdown ', new Date().toISOString());
+  process.kill(process.pid, signal);
 }
