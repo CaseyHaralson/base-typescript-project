0c7bc65dda59bb7838f2dae3c2d989b9a84d5374
Casey <casey.haralson@gmail.com>
2024-02-06 19:36:45 -0700

    removing stupid files added by windows being stupid

--- src/relationalDb/seed/demoEntity.factory.ts
+++
@@ -1,8 +0,0 @@
-import {setSeederFactory} from 'typeorm-extension';
-import {DemoEntity} from '../../entities/demoEntity';
-
-export default setSeederFactory(DemoEntity, (faker) => {
-  const entity = new DemoEntity();
-  entity.name = faker.word.words({count: {min: 1, max: 3}});
-  return entity;
-});
